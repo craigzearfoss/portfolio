16487cef47f294c7d97ac98a1c9720fbec01f2ef
Craig Zearfoss <craigzearfoss@yahoo.com>
2025-09-19 14:47:36 -0400

    added prep_time and total_time to recipes

--- database/migrations/2025_07_25_000700_create_portfolio_projects_table.php
+++ database/migrations/2025_07_25_000700_create_portfolio_projects_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Portfolio\Project;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -18,7 +19,10 @@ return new class extends Migration
             $table->tinyInteger('professional')->default(1);
             $table->tinyInteger('personal')->default(0);
             $table->year('year')->nullable();
-            $table->string('repository')->nullable();
+            $table->string('language', 50)->nullable();
+            $table->string('language_version', 20)->nullable();
+            $table->string('repository_url')->nullable();
+            $table->string('repository_name')->nullable();
             $table->string('link')->nullable();
             $table->string('link_name')->nullable();
             $table->text('description')->nullable();
@@ -37,6 +41,75 @@ return new class extends Migration
 
             $table->unique(['admin_id', 'name'], 'admin_id_name_unique');
             $table->unique(['admin_id', 'slug'], 'admin_id_slug_unique');
+
+            $data = [
+                [
+                    'id'               => 1,
+                    'name'             => 'Multi-guard Framework',
+                    'slug'             => 'multi-guard-framework',
+                    'personal'         => 1,
+                    'year'             => 2025,
+                    'language'         => 'Laravel',
+                    'language_version' => '12.29',
+                    'repository_url'   => 'https://github.com/craigzearfoss/laravel-multi-guard',
+                    'repository_name'  => 'craigzearfoss/laravel-multi-guard',
+                    'link'             => null,
+                    'link_name'        => null,
+                    'description'      => null,
+                    'sequence'         => 0,
+                    'admin_id'         => 1,
+                ],
+                [
+                    'id'               => 2,
+                    'name'             => 'Portfolio Framework',
+                    'slug'             => 'portfolio-framework',
+                    'personal'         => 1,
+                    'year'             => 2025,
+                    'language'         => 'Laravel',
+                    'language_version' => '12.29',
+                    'repository_url'   => 'https://github.com/craigzearfoss/portfolio',
+                    'repository_name'  => 'craigzearfoss/portfolio',
+                    'link'             => null,
+                    'link_name'        => null,
+                    'description'      => null,
+                    'sequence'         => 0,
+                    'admin_id'         => 1,
+                ],
+                [
+                    'id'               => 3,
+                    'name'             => 'Addressable Trait',
+                    'slug'             => 'addressable-trait',
+                    'personal'         => 1,
+                    'year'             => 2025,
+                    'language'         => 'Laravel',
+                    'language_version' => '5.1',
+                    'repository_url'   => 'https://github.com/craigzearfoss/addressable-trait',
+                    'repository_name'  => 'craigzearfoss/addressable-trait',
+                    'link'             => null,
+                    'link_name'        => null,
+                    'description'      => null,
+                    'sequence'         => 0,
+                    'admin_id'         => 1,
+                ],
+                [
+                    'id'               => 4,
+                    'name'             => 'Speedmon',
+                    'slug'             => 'speedmon',
+                    'personal'         => 1,
+                    'year'             => 2025,
+                    'language'         => 'Python',
+                    'language_version' => '3',
+                    'repository_url'   => 'https://github.com/craigzearfoss/speedmon',
+                    'repository_name'  => 'craigzearfoss/speedmon',
+                    'link'             => null,
+                    'link_name'        => null,
+                    'description'      => null,
+                    'sequence'         => 0,
+                    'admin_id'         => 1,
+                ],
+            ];
+
+            Project::insert($data);
         });
     }
 
