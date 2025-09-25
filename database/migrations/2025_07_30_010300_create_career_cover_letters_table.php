f085ffa2538e1d2359050179d76a6b542c723673
Craig Zearfoss <craigzearfoss@yahoo.com>
2025-09-24 20:12:23 -0400

    moved Team and User controllers to Admin directory

--- database/migrations/2025_07_30_010300_create_career_cover_letters_table.php
+++ database/migrations/2025_07_30_010300_create_career_cover_letters_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Career\Application;
+use App\Models\Career\CoverLetter;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -34,6 +35,12 @@ return new class extends Migration
             $table->timestamps();
             $table->softDeletes();
         });
+
+
+        $data = [
+        ];
+
+        CoverLetter::insert($data);
     }
 
     /**
