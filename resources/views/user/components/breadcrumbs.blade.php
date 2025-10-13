f2c17e8f8e61355ad18e523d66b5e43a1b7acb5d
Craig Zearfoss <craigzearfoss@yahoo.com>
2025-10-13 07:13:08 -0400

    added assets

---
+++ resources/views/user/components/breadcrumbs.blade.php
@@ -0,0 +1,14 @@
+<ul>
+    @foreach ($breadcrumbs as $breadcrumb)
+        <li class="is-size-6">
+            @if(!empty($breadcrumb['href']))
+                @include('user.components.link', [
+                    'url'  => $breadcrumb['href'],
+                    'name' => $breadcrumb['name'] ?? '#name#'
+                ])
+            @else
+                <a>{{ $breadcrumb['name'] ?? '#name#' }}</a>
+            @endif
+        </li>
+    @endforeach
+</ul>
---
+++ resources/views/user/layouts/empty.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('user.components.head')
+
+<body>
+
+<div id="app">
+
+    <section class="is-main-section px-4 py-3">
+
+        @include('user.components.messages', [
+            'errorMessages' => $errorMessages ?? [],
+            'success'       => $success ?? null,
+            'error'         => $error ?? null,
+        ])
+
+        <div class="container">
+            @yield('content')
+        </div>
+
+    </section>
+
+    @include('user.components.footer')
+
+</div>
+
+</body>
+
+</html>
