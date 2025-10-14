96cdf595b229faa0bfd4cb7d4b6d9613bc9bddb9
Craig Zearfoss <craigzearfoss@yahoo.com>
2025-10-14 13:52:22 -0400

    updates to InitializeCraigzearfoss.php

--- database/migrations/2025_08_01_050400_create_personal_recipe_steps_table.php
+++ database/migrations/2025_08_01_050400_create_personal_recipe_steps_table.php
@@ -45,102 +45,13 @@ return new class extends Migration
             $table->softDeletes();
         });
 
+        /*
         $data = [
             [
                 'id'            => 1,
                 'recipe_id'     => 1,
                 'step'          => 1,
-                'description'   => 'Preheat oven to 375° F.',
-            ],
-            [
-                'id'            => 2,
-                'recipe_id'     => 1,
-                'step'          => 2,
-                'description'   => 'Combine flour, baking soda and salt in small bowl. Beat butter, granulated sugar, brown sugar and vanilla extract in large mixer bowl until creamy. Add eggs, one at a time, beating well after each addition. Gradually beat in flour mixture. Stir in morsels and nuts. Drop by rounded tablespoon onto ungreased baking sheets.',
-            ],
-            [
-                'id'            => 3,
-                'recipe_id'     => 1,
-                'step'          => 3,
-                'description'   => 'Bake for 9 to 11 minutes or until golden brown. Cool on baking sheets for 2 minutes; remove to wire racks to cool completely.',
-            ],
-            [
-                'id'            => 4,
-                'recipe_id'     => 2,
-                'step'          => 1,
-                'description'   => 'Preheat oven to 380° F.',
-            ],
-            [
-                'id'            => 5,
-                'recipe_id'     => 2,
-                'step'          => 2,
-                'description'   => 'Mix the ingredients in a large bowl and add 3/4 cups of boiling water. Let this sit for a few minutes.',
-            ],
-            [
-                'id'            => 6,
-                'recipe_id'     => 2,
-                'step'          => 3,
-                'description'   => 'Spread out on parchment paper on a baking sheet to the thickness of a cracker.',
-            ],
-            [
-                'id'            => 7,
-                'recipe_id'     => 2,
-                'step'          => 4,
-                'description'   => 'Bake for around 40 minutes until slightly browned and crispy.',
-            ],
-            [
-                'id'            => 8,
-                'recipe_id'     => 3,
-                'step'          => 1,
-                'description'   => 'Put water (or broth) and lentils into a small sauce pan.',
-            ],
-            [
-                'id'            => 9,
-                'recipe_id'     => 3,
-                'step'          => 2,
-                'description'   => 'Bring to a low boil, then reduce heat and simmer for 18 to 22 minutes or until tender for green lentils. (For red lentils simmer for 7 to 10 minutes.)',
-            ],
-            [
-                'id'            => 10,
-                'recipe_id'     => 3,
-                'step'          => 3,
-                'description'   => 'Sauté onion, garlic, and green pepper over medium hear for 4 to 5 minutes.)',
-            ],
-            [
-                'id'            => 11,
-                'recipe_id'     => 3,
-                'step'          => 4,
-                'description'   => 'Combine all ingredients and lentils over medium low heat for 5 to 10 minutes.)',
-            ],
-            [
-                'id'            => 12,
-                'recipe_id'     => 4,
-                'step'          => 1,
-                'description'   => 'Mix all of ingredients in a pot and heat over medium heat.',
-            ],
-            [
-                'id'            => 13,
-                'recipe_id'     => 5,
-                'step'          => 1,
-                'description'   => 'Preheat oven to 375° F.',
-            ],
-            [
-                'id'            => 14,
-                'recipe_id'     => 5,
-                'step'          => 2,
-                'description'   => 'Grind the contents of a 3.75 oz package of John Cope\'s Dried Sweet Corn in a blender or food processor.',
-            ],
-            [
-                'id'            => 15,
-                'recipe_id'     => 5,
-                'step'          => 3,
-                'description'   => 'Add 2 1/2 cups of cold milk, 2 Tbsp. melted butter or margarine, 1 tsp. salt (optional), 1 1/2 Tbsp. sugar, and 2 well beaten eggs. Mix thoroughly',
-            ],
-            [
-                'id'            => 16,
-                'recipe_id'     => 5,
-                'step'          => 4,
-                'description'   => 'Bake in buttered 1.5 or 2 quart casserole dish for 40 to 50 minutes.',
+                'description'   => '',
             ],
         ];
 
@@ -152,6 +63,7 @@ return new class extends Migration
         }
 
         RecipeStep::insert($data);
+        */
     }
 
     /**
