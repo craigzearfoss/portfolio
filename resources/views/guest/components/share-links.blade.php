9ac6445418b3c8982792a77fe41a2d9bed787572
Craig Zearfoss <craigzearfoss@yahoo.com>
2026-03-30 13:17:50 -0400

    added site preview image to default layout

--- resources/views/guest/components/share-links.blade.php
+++ resources/views/guest/components/share-links.blade.php
@@ -1,8 +1,8 @@
 @php
-    $preview_image = $preview_image ?? 'default.png';
+    $previewImage = getShareImage($preview_image ?? 'default.png');
 @endphp
-@if($previewImage = getShareImage($preview_image))
+@if(!empty($previewImage))
     <div style="width: 0; height: 0;">
-        <img src="{{ getShareImage($previewImage) }}" alt="{{ config('app.name') }} preview image" />
+        <img src="{{ $previewImage }}" alt="{{ config('app.name') }} preview image" />
     </div>
 @endif
