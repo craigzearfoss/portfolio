f77cd37b96ccaeb6ac2b43d918f5a86faa960aa6
Craig Zearfoss <craigzearfoss@yahoo.com>
2026-04-09 20:40:54 -0400

    fix to cover letters search panel

--- app/Services/ResumeService.php
+++ app/Services/ResumeService.php
@@ -47,29 +47,29 @@ class ResumeService {
     {
         $this->admin = $admin;
 
-        $this->awards = new Award()->where('owner_id', '=', $this->admin->id)
+        $this->awards = new Award()->where('owner_id', '=', $this->admin['id'])
             ->where('is_public', '=', 1)
             ->where('is_disabled', '=', 0)
             ->orderBy('year')
             ->get();
 
-        $this->certificates = new Certificate()->where('owner_id', '=', $this->admin->id)
+        $this->certificates = new Certificate()->where('owner_id', '=', $this->admin['id'])
             ->where('is_public', '=', 1)
             ->where('is_disabled', '=', 0)
             ->orderBy('received', 'desc')
             ->get();
 
-        $this->educations = new Education()->where('owner_id', '=', $this->admin->id)
+        $this->educations = new Education()->where('owner_id', '=', $this->admin['id'])
             ->where('is_public', '=', 1)
             ->where('is_disabled', '=', 0)
             ->orderBy('graduation_date', 'desc')
             ->get();
 
-        $this->jobs = new Job()->where('owner_id', '=', $this->admin->id)
+        $this->jobs = new Job()->where('owner_id', '=', $this->admin['id'])
             ->orderBy('start_date', 'desc')
             ->get();
 
-        $this->skills = new Skill()->where('owner_id', '=', $this->admin->id)
+        $this->skills = new Skill()->where('owner_id', '=', $this->admin['id'])
             ->where('is_public', '=', 1)
             ->where('is_disabled', '=', 0)
             ->orderBy('sequence')
