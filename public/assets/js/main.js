de8f8ab589126fbdcbcaa55b034cf4dc7c7e667b
Craig Zearfoss <craigzearfoss@yahoo.com>
2026-04-27 09:02:19 -0400

    added toggle show password to password input field

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -62,4 +62,16 @@ document.addEventListener("DOMContentLoaded", () => {
             document.body.removeChild(link);
         })
     });
+
+    const togglePassword = document.querySelector('#togglePassword');
+    const password = document.querySelector('#inputPassword');
+
+    togglePassword.addEventListener('click', function () {
+        // Toggle the type attribute
+        const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+        password.setAttribute('type', type);
+
+        // Toggle the eye / eye-slash icon
+        this.classList.toggle('fa-eye-slash');
+    });
 });
