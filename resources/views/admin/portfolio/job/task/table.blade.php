6945c713ba6c4df49b35e018639a8fa07c27391e
Craig Zearfoss <craigzearfoss@yahoo.com>
2026-05-04 20:41:47 -0400

    fixed resume and cover letter show and edit forms to file fields

--- resources/views/admin/portfolio/job/task/table.blade.php
+++ resources/views/admin/portfolio/job/task/table.blade.php
@@ -60,26 +60,6 @@
 
             </td>
 
-
-
-
-            <td class="is-1" style="white-space: nowrap;">
-
-                <a title="show" class="button is-small px-1 py-0"
-                       href="{{ route('admin.portfolio.job-task.show', $task) }}">
-                    <i class="fa-solid fa-list"></i>
-                </a>
-
-                <a title="edit" class="button is-small px-1 py-0"
-                   href="{{ route('admin.portfolio.job-task.edit', $task) }}">
-                    <i class="fa-solid fa-pen-to-square"></i>
-                </a>
-
-                <button title="remove" type="submit" class="button is-small px-1 py-0">
-                    <i class="fa-solid fa-trash"></i>
-                </button>
-
-            </td>
         </tr>
 
     @endforeach
